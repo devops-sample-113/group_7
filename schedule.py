84f2a61726296896464db681dddf5bc8b524493c
TsengYuFei <sophie99250@gmail.com>
2024-11-02 16:27:49 +0800

    輸入學號成功
    於搜尋頁面輸入學號
    成功傳送到課表頁面並顯示
    (資料會暫存於data.txt，課表顯示完清空)

--- schedule.py
+++ schedule.py
@@ -13,6 +13,13 @@ Swindow=Tk()
 Swindow.title("你的個人課表")
 Swindow.geometry('500x400+390+75')
 
+##獲取輸入資料(學號)
+try:
+    with open("data.txt","r") as file:
+        number=file.read()
+except FileNotFoundError:
+    number="輸入錯誤"
+
 ##個人課表顯示
 #開啟excel
 path='C:\\Users\\User\\Documents\\GitHub\\group_7\\個人課表\\D1234567.xlsx'
@@ -33,5 +40,16 @@ for col in range(6):
 ##返回按鈕
 back=Button(Swindow,text="返回",anchor="s",command=return_search)
 back.grid(row=20,column=5,pady=10)
+
+##顯示下方提示文字
+label_prompt = tk.Label(Swindow, text="          正在查詢：")
+label_prompt.grid(row=25, column=0, padx=10, pady=5)  # 設定位置為第 0 行，第 0 列
+
+label_data = tk.Label(Swindow, text=number)
+label_data.grid(row=25, column=1, padx=10, pady=10)  # 設定位置為第 1 行，第 0 列
+
+##將data.txt暫存資料清空
+with open("data.txt","w") as file:
+        file.write("")
 
 Swindow.mainloop()
--- search_schedule.py
+++ search_schedule.py
@@ -5,6 +5,10 @@ import subprocess
 
 ##前往查詢
 def show_search():
+    #取得輸入資料並前往查詢
+    with open("data.txt","w") as file:
+        file.write(number.get())
+
     search_window.destroy()  # 關閉當前視窗
     subprocess.Popen(["python", "schedule.py"])  # 執行第二個程式
 
@@ -12,6 +16,10 @@ def show_search():
 search_window=Tk()
 search_window.title("個人課表查詢")
 search_window.geometry('500x400+390+75')
+
+##學號輸入框
+number=tk.Entry(search_window)
+number.pack(pady=20)
 
 ##搜尋按鈕
 search = tk.Button(search_window, text="查詢", command=show_search)
